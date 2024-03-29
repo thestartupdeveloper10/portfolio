7029a4ddec10061a896c7c2ec55fbfefb1f82a5b
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-03-29 18:58:15 +0300

    update

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -3,10 +3,15 @@ const nav=document.getElementById("navbar-hamburger")
 const image = document.getElementById('imageChange');
 const bgHeader=document.querySelector(".header-bg")
 const about=document.querySelector(".about")
+const projects=document.querySelector(".projects")
+const contact=document.querySelector(".contact")
+const home=document.querySelector(".home")
 const menuAnimate=document.querySelector(".menu-animate")
 btn.addEventListener("click",toggleMenu)
 about.addEventListener("click",collapseMenu)
-
+projects.addEventListener("click",collapseMenu)
+contact.addEventListener("click",collapseMenu)
+home.addEventListener("click",collapseMenu)
 function collapseMenu() {
     bgHeader.classList.remove("blueBg")
     nav.classList.add("hide")
